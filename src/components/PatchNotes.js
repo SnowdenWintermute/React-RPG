375963f74a5b3bcdfc8adbe8aea968034ac37e2a
SnowdenWintermute <michael.p.silverman@gmail.com>
2018-08-18 17:21:39 -0400

    fixed images stretching

--- src/components/PatchNotes.js
+++ src/components/PatchNotes.js
@@ -11,15 +11,19 @@ class PatchNotes extends Component {
         <h3>Patch Notes</h3>
         </div>
         <div id= "patchNotesList">
+        <p>Patch 1.11</p>
+          <ul>
+          <li>Fixed a bug that caused the start screen images to stretch on some devices.</li>
+          </ul>
         <p>Patch 1.1</p>
-        <ul>
-        <li>Fixed a bug where Heat Lance was healing blue enemies.</li>
-        <li>Fixed a bug where Acid Spines would not deal damage in some cases.</li>
-        <li>Added combat log to death screen.</li>
-        <li>Added patch notes page to welcome screen.</li>
-        <li>Reduced the MP cost of warrior skills from 3 to 2.</li>
-        <li>Increased the maximum Armor Spikes per level from 5 to 10.4</li>
-        </ul>
+          <ul>
+          <li>Fixed a bug where Heat Lance was healing blue enemies.</li>
+          <li>Fixed a bug where Acid Spines would not deal damage in some cases.</li>
+          <li>Added combat log to death screen.</li>
+          <li>Added patch notes page to welcome screen.</li>
+          <li>Reduced the MP cost of warrior skills from 3 to 2.</li>
+          <li>Increased the maximum Armor Spikes per level from 5 to 10.4</li>
+          </ul>
         </div>
       </div>
     </div>
